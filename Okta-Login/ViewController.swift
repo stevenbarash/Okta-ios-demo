c2e035627a6d5cefa93b885fb951fcaea87174e3
Steven Barash <stevenbarash6@gmail.com>
2020-02-06 21:14:36 -0500

    Initial Commit

---
+++ Okta-Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Okta-Login
+//
+//  Created by Steven Barash on 2/6/20.
+//  Copyright © 2020 Steven Barash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
